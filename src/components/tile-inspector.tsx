07aa2dd581e574b82300c662b0c5cfa15e909c9e
Brian Bosh <bbosh@alternateinterior.com>
2022-07-31 12:32:59 -0600

    clarity

--- src/components/tile-inspector.tsx
+++ src/components/tile-inspector.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import { Tile } from '../models/models';
 
+const FACTIONS = ['Hero', 'Villain'];
 export function TileInspector({tile, occupant }: { tile: Tile | null, occupant: any | null }) {
-  return <div style={{position:'absolute', bottom: 0, right: 0, textAlign: 'right', width: '10em',height:'15em'}}>
+  return <div style={{position:'absolute', bottom: 0, right: 0, textAlign: 'right', width: '15em',height:'15em'}}>
+    {occupant && <ul>
+      <li>Name: {occupant.name}</li>
+      <li>Alignment: {FACTIONS[occupant.faction]}</li>
+      <li>Class: {t(occupant.class.name)}</li>
+      <li>AP: {occupant.ap}</li>
+    </ul>}
     {tile && <>
-      <div>occupant: {JSON.stringify(occupant)}</div>
-      <div>cover: {tile.cover}</div>
-      <div>elevation: {tile.elevation}</div>
-      <div>openable: {tile.openable? 'true': 'false'}</div>
-      <div>type: {tile.type}</div>
-      <div>pos: ({tile.x}, {tile.y})</div>
+      <div>Cover: {tile.cover}</div>
+      <div>Elevation: {tile.elevation}</div>
+      <div>Openable: {tile.openable? 'true': 'false'}</div>
+      <div>Position: ({tile.x}, {tile.y})</div>
     </>}
     {!tile && <>
       <div>no selection</div>
     </>}
   </div>
 }
+function t(translation: any): string {
+  return translation['en'];
+}
+
