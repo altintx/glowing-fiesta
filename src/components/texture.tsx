a95e7af2634444fc3157b5b40484aaa2d03a6085
Brian Bosh <bbosh@alternateinterior.com>
2022-08-22 19:30:25 -0600

    jagged little edges

--- src/components/texture.tsx
+++ src/components/texture.tsx
@@ -83,6 +83,24 @@ function roundCell(rowIndex: number, columnIndex: number, map: Tile[][], css: Re
   return newCss;
 }
 
+function raggedOrthogonalCell(rowIndex: number, columnIndex: number, xOffset: number, yOffset: number, map: Tile[][]): boolean {
+  const cell: Tile = map[rowIndex][columnIndex];
+  if(rowIndex === 0 && yOffset < 0) {
+    return false
+  }
+  if(columnIndex === 0 && xOffset < 0) {
+    return false
+  }
+  if(rowIndex === map.length - 1 && yOffset > 0) {
+    return false
+  }
+  if(columnIndex === map[0].length - 1 && xOffset > 0) {
+    return false
+  }
+  const neighbor = map[rowIndex + yOffset][columnIndex + xOffset];
+  return (neighbor.textures[0].graphic !== cell.textures[0].graphic);
+}
+
 export function AnimationTexture({ graphic, direction, ms, width, height }: { graphic: string, direction: string, ms: number, width: string, height: string }) {
   const heightInt = parseInt(height);
   const step = heightInt / (4 + Math.random());
@@ -141,16 +159,65 @@ export function TextureElement({texture, rowIndex, cellIndex, map, tileDimension
 }
 
 export function CompositeTextureElement({ rowIndex, cellIndex, map, tileDimension}: {rowIndex: number, cellIndex: number, map: Tile[][], tileDimension: string}) {
-  let background: string | undefined;
-  if(roundTopLeftCell(rowIndex, cellIndex, map) && rowIndex > 0 && cellIndex > 0) {
-    background = map[rowIndex - 1][cellIndex - 1].textures[0].graphic;
-  } else if(roundTopRightCell(rowIndex, cellIndex, map) && rowIndex > 0 && cellIndex < map[0].length - 1) {
-    background = map[rowIndex - 1][cellIndex + 1].textures[0].graphic;
-  } else if(roundBottomLeftCell(rowIndex, cellIndex, map) && rowIndex < map.length - 1 && cellIndex > 0) {
-    background = map[rowIndex + 1][cellIndex - 1].textures[0].graphic;
-  } else if(roundBottomRightCell(rowIndex, cellIndex, map) && rowIndex < map.length - 1 && cellIndex < map[0].length - 1) {
-    background = map[rowIndex + 1][cellIndex + 1].textures[0].graphic;
-  }
+  const background = useRef<string | undefined>(undefined);
+  const paths = useRef<number[][]>([]);
+  useEffect(() => {
+    if(roundTopLeftCell(rowIndex, cellIndex, map) && rowIndex > 0 && cellIndex > 0) {
+      background.current = map[rowIndex - 1][cellIndex - 1].textures[0].graphic;
+    }
+    if(roundTopRightCell(rowIndex, cellIndex, map) && rowIndex > 0 && cellIndex < map[0].length - 1) {
+      background.current = map[rowIndex - 1][cellIndex + 1].textures[0].graphic;
+    }
+    if(roundBottomLeftCell(rowIndex, cellIndex, map) && rowIndex < map.length - 1 && cellIndex > 0) {
+      background.current = map[rowIndex + 1][cellIndex - 1].textures[0].graphic;
+    }
+    if(roundBottomRightCell(rowIndex, cellIndex, map) && rowIndex < map.length - 1 && cellIndex < map[0].length - 1) {
+      background.current = map[rowIndex + 1][cellIndex + 1].textures[0].graphic;
+    }
+    paths.current = [[0,-1],[1,0],[0,1],[-1,0]].reduce<number[][]>((paths, [xOffset,yOffset]) => {
+      const ragged = raggedOrthogonalCell(rowIndex, cellIndex, xOffset, yOffset, map);
+      if(!background.current && ragged) {
+        background.current = map[rowIndex + yOffset][cellIndex + xOffset].textures[0].graphic;
+      }
+      switch(true) {
+        case (xOffset === 0 && yOffset === -1): // top
+          paths.push([0,0]);
+          if(ragged) {
+            for(let x = 10 * Math.floor(Math.random() * 10); x < 100; x+=Math.floor(10*Math.random())) {
+              paths.push([x,-5+Math.floor(10*Math.random())]);
+            }
+          }
+          break;
+        case (xOffset === 1 && yOffset === 0): // right
+          paths.push([100,0]);
+          if(ragged) {
+            for(let y = 10 * Math.floor(Math.random() * 10); y < 100; y+=Math.floor(10*Math.random())) {
+              paths.push([105-Math.floor(Math.random() * 10),y]);
+            }
+          }
+          break;
+        case (xOffset === 0 && yOffset === 1): // bottom
+          paths.push([100,100]);
+          if(ragged) {
+            for(let x = 10 * Math.floor(Math.random() * 10); x < 100; x+=Math.floor(10*Math.random())) {
+              paths.push([100-x,105-Math.floor(10*Math.random())]);
+            }
+          }
+          break;
+        case (xOffset === -1 && yOffset === 0): // left
+          paths.push([0,100]);
+          if(ragged) {
+            for(let y = 10 * Math.floor(Math.random() * 10); y < 100; y+=Math.floor(10*Math.random())) {
+              paths.push([Math.floor(Math.random() * 10 - 5),100-y]);
+            }
+          }
+          break;
+        
+      }
+      return paths;
+    } , []);
+  }, []);
+
   return <div
     style={{
       position: 'relative',
@@ -159,12 +226,12 @@ export function CompositeTextureElement({ rowIndex, cellIndex, map, tileDimensio
       gridRow: `${rowIndex + 1}`,
       width: tileDimension,
       height: tileDimension,
-      backgroundImage: background? `url(thethirdsequence/${background}512.jpg)`: background,
-      backgroundSize: background? 'cover': background,
+      backgroundImage: background.current? `url(thethirdsequence/${background.current}512.jpg)`: background.current,
+      backgroundSize: background.current? 'cover': background.current,
     }}
     >
       {map[rowIndex][cellIndex].textures.map((texture, index) => {
-        let tile = texture.graphic === background? null: <img
+        let tile = texture.graphic === background.current? null: <img
           key={`texture-${rowIndex}-${cellIndex}-${index}`}
           className="tile"
           src={`thethirdsequence/${texture.graphic}512.jpg`}
@@ -173,6 +240,7 @@ export function CompositeTextureElement({ rowIndex, cellIndex, map, tileDimensio
             return roundCell(rowIndex, cellIndex, map, {
               width: tileDimension,
               height: tileDimension,
+              clipPath: paths.current.length? `polygon(${paths.current.map(([x,y]) => `${x}% ${y}%`).join(',')})`: undefined,
             });
           })()}
         />
